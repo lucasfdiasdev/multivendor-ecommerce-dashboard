a496b598d1e1ecc6d6a39da215be2c9983dc0b16
Lucas Dias <lucasfdiasdev@gmail.com>
2024-05-19 17:03:54 -0300

    fix: remove asChild and add image in avatar src

--- components/global/user-button.tsx
+++ components/global/user-button.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 
 import {
   DropdownMenu,
@@ -15,25 +16,35 @@ import Avatar from "@/components/global/avatar";
 const UserButton = () => {
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Avatar src={""} alt="avatar user" width={40} height={40} />
+      <DropdownMenuTrigger>
+        <Avatar
+          src={
+            "https://images.unsplash.com/photo-1603252109303-2751441dd157?q=80&w=120&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          }
+          alt="avatar user"
+          width={40}
+          height={40}
+        />
       </DropdownMenuTrigger>
 
-      <DropdownMenuContent align="end" className="w-56">
+      <DropdownMenuContent align="end" className="w-56 bg-white">
         <DropdownMenuLabel className="flex flex-col">
           lucasferndias@gmail.com
           <span className="text-xs text-gray-400">admin</span>
         </DropdownMenuLabel>
-        <DropdownMenuSeparator />
 
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
           <DropdownMenuItem>
-            Minha Loja
+            <Link href={"/seller/store"} className="w-full">
+              Minha Loja
+            </Link>
             <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
           </DropdownMenuItem>
           <DropdownMenuItem>
-            Minha conta
+            <Link href={"/seller/profile"} className="w-full">
+              Minha conta
+            </Link>
             <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
           </DropdownMenuItem>
           <DropdownMenuItem>
