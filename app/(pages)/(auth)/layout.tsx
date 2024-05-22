43a699013da39b3d84cb5810c4a45f2434428980
Lucas Dias <lucasfdiasdev@gmail.com>
2024-05-22 01:07:24 -0300

    style: delete component header auth

--- app/(pages)/(auth)/layout.tsx
+++ app/(pages)/(auth)/layout.tsx
@@ -1,10 +1,8 @@
-import HeaderAuth from "@/components/includes/header/header-auth";
-import React from "react";
+"use client";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <HeaderAuth />
       <div className="min-h-screen flex items-center justify-center bg-[#161d31]">
         <div className="p-4 w-[25rem] text-[#d0d2d6]">{children}</div>
       </div>
