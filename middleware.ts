50474d2c609b71d0f49121fa571b5f40403fcba1
Lucas Dias <lucasfdiasdev@gmail.com>
2024-05-22 01:05:26 -0300

    feat: add public and private routes based on access_token

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,43 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+// rotas que o middleware ficará ouvindo
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: [
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/",
+    "/(api|trpc)(.*)",
+    "/admin/:path*",
+    "/seller/:path*",
+  ],
 };
 
-export function middleware(req: NextRequest) {
-  const isAuthenticated = true;
+const publicRoutes = ["/admin-login", "/register", "/login"];
 
-  if (!isAuthenticated) {
-    return NextResponse.redirect(new URL("/admin-login", req.url));
+export function middleware(req: NextRequest) {
+  const url = req.nextUrl;
+  const loginRoute = new URL("/", req.url);
+  const token = req.cookies.get("access_token")?.value;
+  const isPublicRoute = publicRoutes.includes(url.pathname);
+
+  if (
+    url.pathname === "/" ||
+    (url.pathname === "/login" && url.host === process.env.NEXT_PUBLIC_DOMAIN)
+  ) {
+    return NextResponse.rewrite(new URL("/login", req.url));
+  }
+
+  // se existir um token o usuario sera redirecionado para o seller/dashboard
+  if (token && isPublicRoute) {
+    // TODO:
+
+    return NextResponse.redirect(new URL("/seller/dashboard", req.url));
+  }
+
+  if (!token) {
+    if (isPublicRoute) {
+      return NextResponse.next();
+    }
+    return NextResponse.redirect(loginRoute);
   }
 }
