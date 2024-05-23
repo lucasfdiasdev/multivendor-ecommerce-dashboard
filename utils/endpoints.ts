59a43a873360e015174dfc820f2e58b0d37b9064
Lucas Dias <lucasfdiasdev@gmail.com>
2024-05-23 05:30:10 -0300

    feat: add endpoint http getUser

--- utils/endpoints.ts
+++ utils/endpoints.ts
@@ -1,9 +1,15 @@
 const serverUrl = "http://localhost:5000/api";
 const auth = "/auth";
+const user = "/user";
 const authEndpoint = serverUrl + auth;
+const userEndpoint = serverUrl + user;
 
 export const endpoints = {
   admin_login: authEndpoint + "/admin-login",
-  seller_register: authEndpoint + "/seller-register",
   seller_login: authEndpoint + "/seller-login",
+  seller_register: authEndpoint + "/seller-register",
+
+  // user
+  get_user: userEndpoint + "/get-user",
+  get_roles: userEndpoint + "/get-roles",
 };
