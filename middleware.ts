5679a38938c5e68923c0c7892bd428feb384e1a4
Lucas Dias <lucasfdiasdev@gmail.com>
2024-05-23 17:12:52 -0300

    feat: add publicRoutes

--- middleware.ts
+++ middleware.ts
@@ -12,14 +12,21 @@ export const config = {
   ],
 };
 
-const publicRoutes = ["/admin-login", "/register", "/login"];
+const publicRoutes = ["/admin-login", "/register", "/login", "/"];
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
   const url = req.nextUrl;
   const loginRoute = new URL("/", req.url);
   const token = req.cookies.get("access_token")?.value;
+
   const isPublicRoute = publicRoutes.includes(url.pathname);
 
+  // Se houver token, o usuário será redirecionado para /seller/dashboard
+  if (token && isPublicRoute) {
+    return NextResponse.redirect(new URL("/seller/dashboard", req.url));
+  }
+
+  // Se a rota for a raiz ou a página de login e o domínio for o correto, redirecione para a página de login
   if (
     url.pathname === "/" ||
     (url.pathname === "/login" && url.host === process.env.NEXT_PUBLIC_DOMAIN)
@@ -27,13 +34,7 @@ export function middleware(req: NextRequest) {
     return NextResponse.rewrite(new URL("/login", req.url));
   }
 
-  // se existir um token o usuario sera redirecionado para o seller/dashboard
-  if (token && isPublicRoute) {
-    // TODO:
-
-    return NextResponse.redirect(new URL("/seller/dashboard", req.url));
-  }
-
+  // Se não houver token, verifique se a rota é pública e redirecione para a página de login se necessário
   if (!token) {
     if (isPublicRoute) {
       return NextResponse.next();
