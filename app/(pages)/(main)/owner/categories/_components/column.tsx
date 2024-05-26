0f86bc3b426ce97ccd7e770e9819a7de77860d50
Lucas Dias <lucasfdiasdev@gmail.com>
2024-05-26 15:41:38 -0300

    feat: image inserted without data

--- app/(pages)/(main)/owner/categories/_components/column.tsx
+++ app/(pages)/(main)/owner/categories/_components/column.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { ColumnDef } from "@tanstack/react-table";
 
 import CategoryCellAction from "./cell-action";
@@ -7,10 +8,24 @@ import CategoryCellAction from "./cell-action";
 export type CategoryColumn = {
   _id: string;
   name: string;
+  image: string;
   createdAt: string;
 };
 
 export const columns: ColumnDef<CategoryColumn>[] = [
+  {
+    accessorKey: "image",
+    header: "Imagem",
+    cell: ({ row }) => (
+      <Image
+        src={row.original.image}
+        alt={row.original.name}
+        width={30}
+        height={30}
+        className="rounded-md object-cover object-center"
+      />
+    ),
+  },
   {
     accessorKey: "name",
     header: "name",
